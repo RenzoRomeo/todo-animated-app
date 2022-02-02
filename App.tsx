5cdae5be5f4628cbac34040dd464bf213e47e871
renzo <renzoromeo44@gmail.com>
2022-02-01 23:25:56 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppContainer from './src/components/app-container';
+
+import Main from './src/screens/main';
+
+export default function App() {
+  return (
+    <AppContainer>
+      <Main />
+    </AppContainer>
+  );
+}
