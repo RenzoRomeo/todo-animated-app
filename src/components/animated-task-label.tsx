213e8d1ba49de582e9d331d0b5e1e6e6538ff8c6
RenzoRomeo <renzoromeo44@gmail.com>
2022-02-02 02:42:40 -0300

    animated task label (40:59)

---
+++ src/components/animated-task-label.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, memo } from 'react';
+import { Pressable } from 'react-native';
+import { Text, HStack, Box } from 'native-base';
+import Animated, {
+  Easing,
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withSequence,
+  withDelay,
+  interpolateColor
+} from 'react-native-reanimated';
+
+interface Props {
+  strikethrough: boolean;
+  textColor: string;
+  inactiveTextColor: string;
+  onPress?: () => void;
+  children?: React.ReactNode;
+}
+
+const AnimatedBox = Animated.createAnimatedComponent(Box);
+const AnimatedHStack = Animated.createAnimatedComponent(HStack);
+const AnimatedText = Animated.createAnimatedComponent(Text);
+
+const AnimatedTaskLabel = memo((props: Props) => {
+  const { strikethrough, textColor, inactiveTextColor, onPress, children } =
+    props;
+
+  return (
+    <Pressable onPress={onPress}>
+      <AnimatedHStack alignItems="center">
+        <AnimatedText fontSize={19} noOfLines={1} isTruncated px={1}>
+          {children}
+        </AnimatedText>
+        <AnimatedBox
+          position="absolute"
+          h={1}
+          borderBottomWidth={1}
+        ></AnimatedBox>
+      </AnimatedHStack>
+    </Pressable>
+  );
+});
+
+export default AnimatedTaskLabel;
--- src/components/task-item.tsx
+++ src/components/task-item.tsx
@@ -2,12 +2,16 @@ import React, { useCallback } from 'react';
 import { Pressable } from 'react-native';
 import {
   Box,
+  HStack,
+  Text,
   useTheme,
   themeTools,
   useColorModeValue,
   useColorMode
 } from 'native-base';
 import AnimatedCheckbox from 'react-native-checkbox-reanimated';
+
+import AnimatedTaskLabel from './animated-task-label';
 
 interface Props {
   isDone: boolean;
@@ -39,16 +43,31 @@ const TaskItem = (props: Props) => {
   );
 
   return (
-    <Box width={30} height={30} mr={2}>
-      <Pressable onPress={onToggleCheckbox}>
-        <AnimatedCheckbox
-          highlightColor={highlightColor}
-          checkmarkColor={checkmarkColor}
-          boxOutlineColor={boxStroke}
-          checked={isDone}
-        />
-      </Pressable>
-    </Box>
+    <HStack
+      alignItems="center"
+      w="full"
+      px={4}
+      py={2}
+      bg={useColorModeValue('warmGray.50', 'primary.900')}
+    >
+      <Box width={30} height={30} mr={2}>
+        <Pressable onPress={onToggleCheckbox}>
+          <AnimatedCheckbox
+            highlightColor={highlightColor}
+            checkmarkColor={checkmarkColor}
+            boxOutlineColor={boxStroke}
+            checked={isDone}
+          />
+        </Pressable>
+      </Box>
+      <AnimatedTaskLabel
+        textColor={activeTextColor}
+        inactiveTextColor={doneTextColor}
+        strikethrough={isDone}
+      >
+        Task Item
+      </AnimatedTaskLabel>
+    </HStack>
   );
 };
 
